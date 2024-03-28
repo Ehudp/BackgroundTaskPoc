2d7517d0a2be879a1a522fb2d874563cd33d4763
ehud.pinhas <ehud.pinhas@scrdairy.com>
2024-03-28 07:26:02 +0200

    Add PeriodicTimer

--- BackgroundTasks/BackgroundServiceImplementation.cs
+++ BackgroundTasks/BackgroundServiceImplementation.cs
@@ -5,6 +5,7 @@ namespace BackgroundTaskPoc.BackgroundTasks;
 public class BackgroundServiceImplementation : BackgroundService
 {
     private readonly BackgroundServiceSampleData _data;
+    private readonly TimeSpan _period = TimeSpan.FromSeconds(1);
 
     public BackgroundServiceImplementation(BackgroundServiceSampleData data)
     {
@@ -13,11 +14,12 @@ public class BackgroundServiceImplementation : BackgroundService
 
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
-        while (!stoppingToken.IsCancellationRequested)
+        using PeriodicTimer timer = new PeriodicTimer(_period);
+        while (!stoppingToken.IsCancellationRequested && await timer.WaitForNextTickAsync(stoppingToken))
         {
             _data.Data.Add($"Background Service Data, data was added at {DateTime.Now.ToLongTimeString()}");
 
-            await Task.Delay(TimeSpan.FromSeconds(1), stoppingToken);
+            // await Task.Delay(_period, stoppingToken);
         }
     }
 }
