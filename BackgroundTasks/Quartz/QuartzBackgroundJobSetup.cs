bc1b7f8dccb8be5659ee2777efb7f87e8b5b3724
ehud.pinhas <ehud.pinhas@scrdairy.com>
2024-04-11 11:05:17 +0300

    Add IANA Format

--- BackgroundTasks/Quartz/QuartzBackgroundJobSetup.cs
+++ BackgroundTasks/Quartz/QuartzBackgroundJobSetup.cs
@@ -13,8 +13,8 @@ public class QuartzBackgroundJobSetup : IConfigureOptions<QuartzOptions>
             .AddTrigger(trigger => trigger
                     .ForJob(jobKey)
                      .WithSchedule(CronScheduleBuilder
-                .DailyAtHourAndMinute(11, 02)
-                .InTimeZone(TimeZoneInfo.FindSystemTimeZoneById("Israel Standard Time"))));
+                .DailyAtHourAndMinute(11, 05)
+                .InTimeZone(TimeZoneInfo.FindSystemTimeZoneById("Asia/Jerusalem"))));
         //.WithCronSchedule("0 43 7 ? * *")); // Cron expression for 10:20 AM in Israel's local time (UTC+3)
         //.WithCronSchedule("0 0 8 * * ?")); // Cron expression for 8:00 AM every day
         //.WithSimpleSchedule(schedule =>schedule.WithIntervalInSeconds(1).RepeatForever()));
