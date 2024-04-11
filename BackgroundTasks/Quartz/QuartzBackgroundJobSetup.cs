686b3e60f46166ade4467410ed232d4c665235b5
ehud.pinhas <ehud.pinhas@scrdairy.com>
2024-04-11 12:54:47 +0300

    Add CronSchedule

--- BackgroundTasks/Quartz/QuartzBackgroundJobSetup.cs
+++ BackgroundTasks/Quartz/QuartzBackgroundJobSetup.cs
@@ -15,12 +15,20 @@ public class QuartzBackgroundJobSetup : IConfigureOptions<QuartzOptions>
                      .WithSchedule(CronScheduleBuilder
                 .DailyAtHourAndMinute(11, 05)
                 .InTimeZone(TimeZoneInfo.FindSystemTimeZoneById("Asia/Jerusalem"))));
+
         //.WithCronSchedule("0 43 7 ? * *")); // Cron expression for 10:20 AM in Israel's local time (UTC+3)
         //.WithCronSchedule("0 0 8 * * ?")); // Cron expression for 8:00 AM every day
         //.WithSimpleSchedule(schedule =>schedule.WithIntervalInSeconds(1).RepeatForever()));
         // .WithCronSchedule("0/1 * * * * ?")); // Cron expression for every second
 
-
+        // options
+        //       .AddJob<QuartzBackgroundJobImplementation>(jobBuilder => jobBuilder
+        //           .WithIdentity(jobKey))
+        //       .AddTrigger(triggerBuilder => triggerBuilder
+        //           .ForJob(jobKey)
+        //           .WithSchedule(CronScheduleBuilder
+        //               .CronSchedule("0 54 12 ? * *")
+        //               .InTimeZone(TimeZoneInfo.FindSystemTimeZoneById("Asia/Jerusalem"))));
     }
 }
 
