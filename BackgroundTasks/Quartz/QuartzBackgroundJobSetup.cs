5263746c3eafe1895aae2175ce997a6063c74814
ehud.pinhas <ehud.pinhas@scrdairy.com>
2024-04-11 11:03:04 +0300

    Add Time Zone

--- BackgroundTasks/Quartz/QuartzBackgroundJobSetup.cs
+++ BackgroundTasks/Quartz/QuartzBackgroundJobSetup.cs
@@ -10,11 +10,16 @@ public class QuartzBackgroundJobSetup : IConfigureOptions<QuartzOptions>
         var jobKey = JobKey.Create(nameof(QuartzBackgroundJobImplementation));
         options
             .AddJob<QuartzBackgroundJobImplementation>(jobBuilder => jobBuilder.WithIdentity(jobKey))
-            .AddTrigger(trigger =>
-                trigger
+            .AddTrigger(trigger => trigger
                     .ForJob(jobKey)
-                                        //.WithSimpleSchedule(schedule =>schedule.WithIntervalInSeconds(1).RepeatForever()));
-                                        .WithCronSchedule("0/1 * * * * ?")); // Cron expression for every second
+                     .WithSchedule(CronScheduleBuilder
+                .DailyAtHourAndMinute(11, 02)
+                .InTimeZone(TimeZoneInfo.FindSystemTimeZoneById("Israel Standard Time"))));
+        //.WithCronSchedule("0 43 7 ? * *")); // Cron expression for 10:20 AM in Israel's local time (UTC+3)
+        //.WithCronSchedule("0 0 8 * * ?")); // Cron expression for 8:00 AM every day
+        //.WithSimpleSchedule(schedule =>schedule.WithIntervalInSeconds(1).RepeatForever()));
+        // .WithCronSchedule("0/1 * * * * ?")); // Cron expression for every second
+
 
     }
 }
